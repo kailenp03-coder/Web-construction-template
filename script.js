9f2922e459ef5aa32daa66b8627686935f194f87
kailenp03-coder <kailenp03@gmail.com>
2026-02-24 10:57:37 +0200

    fix css

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ document.addEventListener("DOMContentLoaded", initSite);
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector("header");
 
-if (hamburger) {
+if (hamburger && header) {
   hamburger.addEventListener("click", () => {
     header.classList.toggle("menu-open");
   });
